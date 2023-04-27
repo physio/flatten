4c7fc10577f6c14e8d31cf227c1cf227e9f203ad
Gallo Mauro <mauro.a.gallo@avanade.com>
2023-04-27 22:57:30 +0200

    feat: search by path

--- src/Searcher.class.ts
+++ src/Searcher.class.ts
@@ -2,42 +2,60 @@ import { SearcherRepositoryInterface } from './interfaces/SearcherRepository.int
 import _ from 'lodash';
 import { FlattenerClass } from './Flatten.class';
 
-export class Searcher implements SearcherRepositoryInterface {
+export class Searcher {
   private static instance: Searcher = null;
   private repository: Object[] = [];
-  public static getRepository(): Searcher {
+
+  /**
+   * Description
+   * @returns {any}
+   */
+  all(): Object[] {
+    return this.repository;
+  }
+
+  /**
+   * Description: search property in the repository
+   * @param {any} searchTerm:string
+   * @returns {any}
+   */
+  public search(searchTerm: string): Object[] {
+    if (this.repository.hasOwnProperty(searchTerm)) {
+      return this.repository[searchTerm];
+    } else {
+      throw new Error('search term not found.');
+    }
+    /*
+    const filteredCollection = _.filter(this.repository, (item: string) => {
+      const values = _.flatMapDeep(item, (value) => (_.isArray(value) ? value : [value]));
+      return _.some(values, (value) => _.includes(_.toLower(value), _.toLower(searchTerm)));
+    });
+
+    return filteredCollection;*/
+  }
+
+  /**
+   * Description: get the collection
+   * @param {any} collection:string
+   * @returns {any}
+   */
+  public static getCollection(): Searcher {
     if (this.instance == null) {
       this.instance = new Searcher();
     }
     return this.instance;
   }
 
-  all(): Object[] {
-    return this.repository;
-  }
-
-  getItem(index: number): Object {
-    return { id: index };
-  }
-
-  public search(searchTerm: string): Object[] {
-    const filteredCollection = _.filter(this.repository, (item) => {
-      const values = _.flatMapDeep(item, (value) => (_.isArray(value) ? value : [value]));
-      return _.some(values, (value) => _.includes(_.toLower(value), _.toLower(searchTerm)));
-    });
-
-    return filteredCollection;
-  }
-
-  getCollection(collection: string): Object[] {
-    return [{}];
-  }
-
+  /**
+   * Description: populate the repository
+   * @param {any} jsonData:any
+   * @returns {any}
+   */
   populate(jsonData: any): void {
     try {
       this.repository = FlattenerClass.flatten(jsonData);
     } catch (err) {
-      throw new Error('Strapi file configuration not valid.');
+      throw new Error('data not valid.');
     }
   }
 }
--- tests/searcher.class.spec.ts
+++ tests/searcher.class.spec.ts
@@ -3,27 +3,134 @@ import { Searcher } from '../src/Searcher.class';
 describe('SearchClass', () => {
   it('Should populate the repository', () => {
     const obj = {
-      EnableSymbolName: 'production',
-      SamplesBefore: 30,
+      name: 'Mario',
+      surname: 'Rossi',
+      age: 30,
+      address: {
+        city: 'Modena',
+        state: 'IT',
+      },
     };
 
     const searcher = new Searcher();
     const aspect = {
-      EnableSymbolName: 'production',
-      SamplesBefore: 30,
+      name: 'Mario',
+      surname: 'Rossi',
+      age: 30,
+      'address.city': 'Modena',
+      'address.state': 'IT',
     };
     searcher.populate(obj);
     expect(searcher.all()).toEqual(aspect);
   });
 
-  it('Should return the repository', () => {
+  it('Should return the property of an object without array', () => {
     const obj = {
-      EnableSymbolName: 'production',
-      SamplesBefore: 30,
+      name: 'Mario',
+      surname: 'Rossi',
+      age: 30,
+      address: {
+        city: 'Modena',
+        state: 'IT',
+      },
     };
+    const aspectAge = 30;
+    const aspectCity = 'Modena';
 
     const searcher = new Searcher();
     searcher.populate(obj);
-    expect(searcher.search('SamplesBefore')).toEqual(obj);
+    expect(searcher.search('age')).toEqual(aspectAge);
+    expect(searcher.search('address.city')).toEqual(aspectCity);
+  });
+
+  it('Should return the property of an object with array', () => {
+    const obj = {
+      name: 'Mario',
+      surname: 'Rossi',
+      hobbies: ['reading', 'cooking'],
+    };
+    const aspectReading = 'reading';
+    const aspectCooking = 'cooking';
+
+    const searcher = new Searcher();
+    searcher.populate(obj);
+    expect(searcher.search('hobbies[0]')).toEqual(aspectReading);
+    expect(searcher.search('hobbies[1]')).toEqual(aspectCooking);
+  });
+
+  it('Should return the property of an array', () => {
+    const obj = ['reading', 'cooking'];
+    const aspectReading = 'reading';
+    const aspectCooking = 'cooking';
+
+    expect(() => {
+      const searcher = new Searcher();
+      searcher.populate(obj);
+      searcher.search('[0]');
+    }).toThrow('search term not found.');
+  });
+
+  it('Should return the property of an object with array', () => {
+    const obj = {
+      name: 'Mario',
+      hobbies: [
+        {
+          name: 'reading',
+        },
+        {
+          name: 'cooking',
+        },
+      ],
+    };
+    const aspectReading = 'reading';
+    const aspectCooking = 'cooking';
+
+    const searcher = new Searcher();
+    searcher.populate(obj);
+    expect(searcher.search('hobbies[0].name')).toEqual(aspectReading);
+    expect(searcher.search('hobbies[1].name')).toEqual(aspectCooking);
+  });
+
+  it('Should return the property of an object with array', () => {
+    const obj = {
+      name: 'Mario',
+      hobbies: [
+        {
+          name: 'reading',
+          test: [
+            {
+              name: 'test',
+            },
+          ],
+        },
+        {
+          name: 'cooking',
+          secondTest: ['mario', 'Ross'],
+        },
+      ],
+    };
+    const firstResult = 'test';
+    const secondResult = 'Ross';
+
+    const searcher = new Searcher();
+    searcher.populate(obj);
+    expect(searcher.search('hobbies[0].test[0].name')).toEqual(firstResult);
+    expect(searcher.search('hobbies[1].secondTest[1]')).toEqual(secondResult);
+  });
+
+  it('Should return the empty repository', () => {
+    const obj = {
+      name: 'Mario',
+      hobbies: [
+        {
+          name: 'reading',
+        },
+        {
+          name: 'cooking',
+        },
+      ],
+    };
+    const aspect = { repository: [] };
+    expect(Searcher.getCollection()).toEqual(aspect);
   });
 });
